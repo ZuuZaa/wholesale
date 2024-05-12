1a347f3f6344ef3d62e62802566f4a6e5be2ce43
ZuuZaa <zumbaghirova@gmail.com>
2024-05-12 18:31:23 +0400

    feat: product cards for category page

---
+++ src/components/cards/card-with-faverite-button.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardWithFaveriteButton = () => {
+  return (
+    <div>card-with-faverite</div>
+  )
+}
+
+export default CardWithFaveriteButton;
---
+++ src/components/cards/index.js
@@ -0,0 +1,1 @@
+export {default} from './card-with-faverite-button'
