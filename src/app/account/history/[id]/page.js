20e20fb37ad079c410a0c1002a402171353464f3
ZuuZaa <zumbaghirova@gmail.com>
2024-08-05 10:08:45 +0400

    feat: add common fetch for order details page

--- src/app/account/history/[id]/page.js
+++ src/app/account/history/[id]/page.js
@@ -1,8 +1,7 @@
 "use client";
 
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
-import Menu from "../../menu";
 import MobilePageLayout from "@/components/layout/mobile-page-layout";
 import CardFrame from "@/components/cards/card-frame";
 
@@ -12,109 +11,33 @@ import { ORDER_STATUS, PAYMENT_TYPE, SHIPPING_TYPE } from "@/constans";
 import Loading from "@/components/loading";
 import BottomFixedCard from "@/components/cards/bottom-fixed-card";
 
+
 export default function Account() {
   const pathname = useParams();
   const id = pathname.id;
   const params = new URLSearchParams();
-  const [isLoading, setIsLoading] = useState(true);
   params.append("Id", id);
-  const mainFunc = async () => {
-    let status;
-    let fav_data = [];
 
-    const fetchData = async () => {
-      let token = "";
-      if (typeof localStorage !== "undefined") {
-        token = localStorage.getItem("jwtToken");
+  const [orderDetails, setOrderDetails] = useState({})
+  const [orderedProducts, setOrderedProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      setIsLoading(true);
+      try {
+        const result = await fetchData("getOrderDetails");
+        setOrderDetails(result.OrderDetails[0]);
+        setOrderedProducts(result.OrderProducts);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setIsLoading(false);
       }
-      let response = await fetch(
-        `https://api.wscshop.co.uk/api/profile/get-order-details?${params.toString()}`,
-        {
-          method: "GET",
-          dataType: "json",
-          headers: {
-            Accept: "application/json, text/plain",
-            "Content-Type": "application/json;charset=UTF-8",
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-
-      const resp = await response.json();
-      status = resp.status;
-      fav_data = resp.output;
-      console.log(fav_data);
-      //return resp.output;
     };
 
-    await fetchData();
-
-    if (status === 401) {
-      try {
-        let token = "";
-        let refreshToken = "";
-        if (typeof localStorage !== "undefined") {
-          token = localStorage.getItem("jwtToken");
-          refreshToken = localStorage.getItem("refreshToken");
-        }
-        console.log(token);
-        console.log(refreshToken);
-        let response = await fetch(
-          `https://api.wscshop.co.uk/api/account/refresh-token?userRefreshToken=${refreshToken}`,
-          {
-            method: "POST",
-            dataType: "json",
-            headers: {
-              Accept: "application/json, text/plain",
-              "Content-Type": "application/json;charset=UTF-8",
-              Authorization: "Bearer " + token,
-            },
-          }
-        );
-        console.log(response);
-        const resp = await response.json();
-
-        if (resp.status !== 400) {
-          if (typeof localStorage !== "undefined") {
-            localStorage.setItem("refreshToken", resp.output.refreshToken);
-            localStorage.setItem("jwtToken", resp.output.token);
-          }
-
-          await fetchData();
-        } else {
-          if (typeof window !== "undefined") {
-            window.location.href = "/login";
-          }
-          //go to login page
-          //alert(1)
-        }
-      } catch {
-        console.log("error");
-      }
-    } else {
-      return fav_data;
-    }
-  };
-
-  const [data, setData] = useState({
-    userOrders: [],
-    orderDetails: [],
-    orderProducts: [],
-  });
-  useEffect(() => {
-    async function fetchDataAsync() {
-      const fetchedData = await mainFunc();
-      setData(fetchedData);
-      setIsLoading(false);
-    }
     fetchDataAsync();
   }, []);
-
-  const userOrders = data.userOrders;
-  const orderDetails = data.orderDetails;
-  const details = orderDetails[0];
-  console.log(details, orderDetails);
-  const orderProducts = data.orderProducts;
 
   return (
     <main>
@@ -131,48 +54,48 @@ export default function Account() {
                 <div className="order-details-card flex flex-col gap-2 p-1">
                   <div className="flex justify-between">
                     <span>Order No</span>
-                    <b>{`#${details?.id}`}</b>
+                    <b>{`#${orderDetails?.Id}`}</b>
                   </div>
                   <div className="flex justify-between">
                     <span>Order Date</span>
-                    <b>{dateNormalizer(details?.soldDate)}</b>
+                    <b>{dateNormalizer(orderDetails?.SoldDate)}</b>
                   </div>
                   <div className="flex justify-between">
                     <span>Payment Type</span>
-                    <b>{PAYMENT_TYPE[details?.paymentType]?.name}</b>
+                    <b>{PAYMENT_TYPE[orderDetails?.PaymentType]?.name}</b>
                   </div>
                   <div className="flex justify-between">
                     <span>Shipping Type</span>
-                    <b>{SHIPPING_TYPE[details?.shippingType]?.name}</b>
+                    <b>{SHIPPING_TYPE[orderDetails?.ShippingType]?.name}</b>
                   </div>
                 </div>
               </CardFrame>
               <CardFrame>
                 <ul className="order-products">
-                  {orderProducts?.map((order) => (
-                    <li key={order.id}>
+                  {orderedProducts?.map((order) => (
+                    <li key={order.Id}>
                       <div className="flex gap-3 items-center py-2">
                         <figure>
                           <img
-                            src={order.productImage}
+                            src={order.ProductImage}
                             onError={(e) =>
-                              order.catImage && (e.target.src = order.catImage)
+                              order.CatImage && (e.target.src = order.CatImage)
                             }
                             alt="order"
                           />
                         </figure>
                         <div className="grow flex flex-col justify-between">
-                          <h3>{order.productName}</h3>
+                          <h3>{order.ProductName}</h3>
                           <p>
-                            {`Status: ${ORDER_STATUS[order.status].name}`}{" "}
+                            {`Status: ${ORDER_STATUS[order.Status].name}`}{" "}
                             &#9432;
                           </p>
                           <div className="flex justify-between">
                             <p className="flex gap-2">
-                              <span>{`Quantity: ${order.quantity}`}</span>
-                              <span>{`Price: ₤${order.price}`}</span>
+                              <span>{`Quantity: ${order.Quantity}`}</span>
+                              <span>{`Price: ₤${order.Price}`}</span>
                             </p>
-                            <b>{`₤${order.total}`}</b>
+                            <b>{`₤${order.Total}`}</b>
                           </div>
                         </div>
                       </div>
@@ -185,23 +108,23 @@ export default function Account() {
                   <div className="flex flex-col gap-2 p-1">
                     <div className="flex justify-between">
                       <span>Subtotal</span>
-                      <b>{`₤${details?.amount}`}</b>
+                      <b>{`₤${orderDetails?.Amount}`}</b>
                     </div>
                     <div className="flex justify-between">
                       <span>Delivery Fee</span>
-                      <b>{`₤${details?.deliveryFee}`}</b>
+                      <b>{`₤${orderDetails?.DeliveryFee}`}</b>
                     </div>
                     <div className="flex justify-between">
                       <span>Discount</span>
-                      <b>{`₤${details?.discount}`}</b>
+                      <b>{`₤${orderDetails?.Discount}`}</b>
                     </div>
                     <div className="flex justify-between">
                       <span>VAT</span>
-                      <b>{`₤${details?.vat}`}</b>
+                      <b>{`₤${orderDetails?.Vat}`}</b>
                     </div>
                     <div className="flex justify-between">
                       <b>Total</b>
-                      <b className="color-green">{`₤${details?.total}`}</b>
+                      <b className="color-green">{`₤${orderDetails?.Total}`}</b>
                     </div>
                   </div>
                 </CardFrame>
