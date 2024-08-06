393918c35d13a3ae2904b911a2636e1e5f502cb7
ZuuZaa <zumbaghirova@gmail.com>
2024-08-06 09:05:45 +0400

    feat: add fetch for wishlist

--- src/app/wishlist/page.js
+++ src/app/wishlist/page.js
@@ -5,180 +5,16 @@ import FavoriteCard from "@/components/cards/favorite-card";
 import MobilePageLayout from "@/components/layout/mobile-page-layout";
 import SearchBar from "@/components/search-bar";
 
-const mainFunc = async () => {
-  let status;
-  let fav_data = [];
-
-  const fetchData = async () => {
-    let token = "";
-    if (typeof localStorage !== "undefined") {
-      token = localStorage.getItem("jwtToken");
-    }
-    let response = await fetch(
-      `https://api.wscshop.co.uk/api/favorites/get-index`,
-      {
-        method: "GET",
-        dataType: "json",
-        headers: {
-          Accept: "application/json, text/plain",
-          "Content-Type": "application/json;charset=UTF-8",
-          Authorization: "Bearer " + token,
-        },
-      }
-    );
-    const resp = await response.json();
-    status = resp.status;
-    console.log(status);
-    fav_data = resp.output;
-    //return resp.output;
-  };
-
-  await fetchData();
-
-  if (status === 401) {
-    try {
-      let token = "";
-      let refreshToken = "";
-      if (typeof localStorage !== "undefined") {
-        token = localStorage.getItem("jwtToken");
-        refreshToken = localStorage.getItem("refreshToken");
-      }
-      console.log(token);
-      console.log(refreshToken);
-      let response = await fetch(
-        `https://api.wscshop.co.uk/api/account/refresh-token?userRefreshToken=${refreshToken}`,
-        {
-          method: "POST",
-          dataType: "json",
-          headers: {
-            Accept: "application/json, text/plain",
-            "Content-Type": "application/json;charset=UTF-8",
-            Authorization: "Bearer " + token,
-          },
-        }
-      );
-      console.log(response);
-      const resp = await response.json();
-
-      if (resp.status !== 400) {
-        if (typeof localStorage !== "undefined") {
-          localStorage.setItem("refreshToken", resp.output.refreshToken);
-          localStorage.setItem("jwtToken", resp.output.token);
-        }
-
-        await fetchData();
-      } else {
-        if (typeof window !== "undefined") {
-          window.location.href = "/login";
-        }
-        //go to login page
-        //alert(1)
-      }
-    } catch {
-      console.log("error");
-    }
-  } else {
-    return fav_data;
-  }
-};
-let removeFavorite = async (event) => {
-  let prodid = event.currentTarget.getAttribute("id");
-  let status;
-  let token = "";
-  if (typeof localStorage !== "undefined") {
-    token = localStorage.getItem("jwtToken");
-  }
-  let remote_button = event.currentTarget;
-  //e.preventDefault();
-  try {
-    const res = await fetch(
-      "https://api.wscshop.co.uk/api/favorites/remove-favorite",
-      {
-        method: "POST",
-        headers: {
-          Accept: "application/json, text/plain",
-          "Content-Type": "application/json;charset=UTF-8",
-          Authorization: "Bearer " + token,
-        },
-        body: JSON.stringify({
-          Id: prodid,
-        }),
-      }
-    );
-    const resJson = await res.json();
-    //if (res.status === 200) {
-
-    status = resJson.status;
-
-    if (status === 401) {
-      try {
-        let token = "";
-        let refreshToken = "";
-        if (typeof localStorage !== "undefined") {
-          token = localStorage.getItem("jwtToken");
-          refreshToken = localStorage.getItem("refreshToken");
-        }
-        let response = await fetch(
-          `https://api.wscshop.co.uk/api/account/refresh-token?userRefreshToken=${refreshToken}`,
-          {
-            method: "POST",
-            dataType: "json",
-            headers: {
-              Accept: "application/json, text/plain",
-              "Content-Type": "application/json;charset=UTF-8",
-              Authorization: "Bearer " + token,
-            },
-          }
-        );
-        const resp = await response.json();
-        if (resp.status !== 400) {
-          if (typeof localStorage !== "undefined") {
-            localStorage.setItem("refreshToken", resp.output.refreshToken);
-            localStorage.setItem("jwtToken", resp.output.token);
-          }
-
-          await removeFavorite();
-        } else {
-          if (typeof window !== "undefined") {
-            window.location.href = "/login";
-          }
-          //go to login page
-          //alert(1)
-        }
-      } catch {
-        console.log("error");
-      }
-    } else {
-      remote_button.parentElement.remove();
-      if (resJson.output.favorites.length == 0) {
-        console.log("empty");
-        document.getElementById("favorites_div").innerHTML =
-          "<p>There is no favorite product!</p>";
-      }
-      //setMessage("Added");
-    }
-    //}
-    //else
-    //{
-    // console.log("Some error occured");
-    //}
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-export default function Wishlist() {
-  //mainFunc();
+const Wishlist = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [favorites, setFavorites] = useState([]);
   const [filteredCarts, setFilteredCarts] = useState([]);
   const [searchKey, setSearchKey] = useState("");
-  const [activePage, setActivePage] = useState(1);
- // const [totalPages, setTotalPages] = useState(null);
- // const [currentFavorites, setCurrentFavorites] = useState([]);
-  
+  //const [activePage, setActivePage] = useState(1);
+  // const [totalPages, setTotalPages] = useState(null);
+  // const [currentFavorites, setCurrentFavorites] = useState([]);
 
-  const itemsPerPage = 10;
+  // const itemsPerPage = 10;
 
   // const setPages = (page) => {
   //   setActivePage(page);
@@ -196,21 +32,27 @@ export default function Wishlist() {
   };
 
   useEffect(() => {
+    const fetchDataAsync = async () => {
+      setIsLoading(true);
+      try {
+        const result = await fetchData("getFavorites", true);
+        setFavorites(result.favorites);
+      } catch (error) {
+        console.error(error.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchDataAsync();
+  }, []);
+
+  useEffect(() => {
     const filteredCarts = favorites?.filter((item) =>
       item.name.toLowerCase().includes(searchKey.trim())
     );
     setFilteredCarts(filteredCarts);
   }, [searchKey]);
-
-  useEffect(() => {
-    async function fetchDataAsync() {
-      const data = await mainFunc();
-      setFavorites(data.favorites);
-      setIsLoading(false);
-    }
-    fetchDataAsync();
-  }, []);
-
 
   return (
     <main>
@@ -256,4 +98,6 @@ export default function Wishlist() {
       )}
     </main>
   );
-}
+};
+
+export default Wishlist;
