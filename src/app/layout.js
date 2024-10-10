ea123a778690d03d8d1faa05ad25d278a71dbd6e
ZuuZaa <zumbaghirova@gmail.com>
2024-10-10 17:20:03 +0400

    delete: remove import icomonn style file from layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,5 @@
 import { Roboto } from "next/font/google";
 import "./globals.css";
-import '../assets/icomoon/style.css';
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { TotalQuantityProvider } from "@/context/total-quantity-context";
